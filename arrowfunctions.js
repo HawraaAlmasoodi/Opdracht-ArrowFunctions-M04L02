30669fac4673af0b702563626d48d044ad027a63
Hawraa Al Masoodi <hawraaalmasoodi@MacBook-Air-van-Hawraa.local>
2020-08-19 13:10:36 +0200

    arrow functions

---
+++ arrowfunctions.js
@@ -0,0 +1,35 @@
+// Deel A:
+
+// zet de functie om naar een arrow functie
+/*const ikRockArrowFunctions = function () {
+  console.log("Joe, ik rock de arrow functions!");
+};
+
+ikRockArrowFunctions()*/
+
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+ikRockArrowFunctions();
+
+// Deel B:
+
+// zet de functie om naar een arrow functie op 1 regel
+/*const fivePlusSeven = function () {
+  return 5 + 7
+};
+
+fivePlusSeven(); */
+
+const fivePlusSeven = () => 5 + 7
+fivePlusSeven();
+
+// Deel C:
+sum => 1 + 2
+
+// Deel D:
+const myFunction = (a, b) => a + b
+
+// Deel E:
+const addFive = a => a + 5
+
+// Deel F:
+const createObject = () => ({ greeting: "hoi" })
